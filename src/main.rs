1f4ad854d9973d1e0694199e406f8acf293ce642
MustacheNinja23 <michaelduffielindsay@gmail.com>
2025-09-27 13:16:31 -0400

    blif parsing demo

--- src/main.rs
+++ src/main.rs
@@ -19,28 +19,74 @@ fn print_blif_components(list: Vec<ParsedPrimitive>) {
     for x in list.into_iter() {
         match x {
             ParsedPrimitive::NOP => println!("NOP"),
-            ParsedPrimitive::Input { name } => println!("{}", name),
-            ParsedPrimitive::Output { name } => println!("{}", name),
+
+            ParsedPrimitive::Input { name } => println!("--Input: {}", name),
+
+            ParsedPrimitive::Output { name } => println!("--Output: {}", name),
+
             ParsedPrimitive::Lut {
                 inputs,
                 output,
                 table,
-            } => println!("{}", output),
-            ParsedPrimitive::Gate { c, d, q, r, e } => println!("{}", c),
+            } => {
+                println!("----Lut----");
+                //inputs
+                println!("--Inputs--");
+                inputs.iter().for_each(|a| println!("{}", a));
+
+                //output
+                println!("Output: {}", output);
+
+                //table
+                println!("--Table--");
+                let lut_table = table.iter().flatten();
+                lut_table.for_each(|a| println!("{}", a));
+            }
+
+            ParsedPrimitive::Gate { c, d, q, r, e } => {
+                println!("----Gate----");
+
+                //whatever these are
+                println!("c: {}", c);
+                println!("d: {}", d);
+                println!("q: {}", q);
+                println!("r: {}", r.unwrap());
+                println!("e: {}", e.unwrap());
+            }
+
             ParsedPrimitive::Latch {
                 input,
                 output,
                 control,
                 init,
-            } => println!("{}", input),
-            ParsedPrimitive::Subckt { name, conns } => println!("Subckt name: {}", name),
+            } => {
+                println!("----Latch----");
+                println!("Input: {}", input);
+                println!("Output: {}", output);
+
+                // no idea what this is for
+                match init {
+                    primitives::LatchInit::ZER0 => println!("0"), // Zero spelled with a 0
+                    primitives::LatchInit::ONE => println!("1"),
+                    primitives::LatchInit::DONTCARE => println!("2"),
+                    primitives::LatchInit::UNKNOWN => println!("3"),
+                }
+            }
+
+            ParsedPrimitive::Subckt { name, conns } => {
+                println!("----Subckt----");
+                println!("Name: {}", name);
+                conns.iter().for_each(|a| println!("{}", a.0));
+            }
+
             ParsedPrimitive::Module {
                 name,
                 inputs,
                 outputs,
                 elems,
             } => {
-                println!("Module name: {}", name);
+                println!("----Module----");
+                println!("Name: {}", name);
 
                 //inputs
                 print!("Inputs:");
