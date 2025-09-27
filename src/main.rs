3f68ff75b61fd0c08991543d8d370a812b947b28
MustacheNinja23 <michaelduffielindsay@gmail.com>
2025-09-27 12:28:36 -0400

    blif parsing demo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,61 @@
 use blif_parser::*;
 use primitives::ParsedPrimitive;
+use std::path::{self, Path};
 
+#[allow(unused_variables)]
 fn main() {
-    let list = parser::parse_blif_file("~/veristone/counter.blif");
-    for x in list.ok().unwrap().into_iter() {
+    let path = Path::new("counter.blif");
+    let binding = path::absolute(path).unwrap().into_os_string();
+    let full_path = binding.to_str().unwrap();
+
+    println!("{}", full_path);
+
+    let list = parser::parse_blif_file(full_path).unwrap();
+    print_blif_components(list);
+}
+
+#[allow(unused_variables)]
+fn print_blif_components(list: Vec<ParsedPrimitive>) {
+    for x in list.into_iter() {
         match x {
-            ParsedPrimitive::Gate { c, d, q, r, e } => {
-                print!("{}", c);
+            ParsedPrimitive::NOP => println!("NOP"),
+            ParsedPrimitive::Input { name } => println!("{}", name),
+            ParsedPrimitive::Output { name } => println!("{}", name),
+            ParsedPrimitive::Lut {
+                inputs,
+                output,
+                table,
+            } => println!("{}", output),
+            ParsedPrimitive::Gate { c, d, q, r, e } => println!("{}", c),
+            ParsedPrimitive::Latch {
+                input,
+                output,
+                control,
+                init,
+            } => println!("{}", input),
+            ParsedPrimitive::Subckt { name, conns } => println!("Subckt name: {}", name),
+            ParsedPrimitive::Module {
+                name,
+                inputs,
+                outputs,
+                elems,
+            } => {
+                println!("Module name: {}", name);
+
+                //inputs
+                print!("Inputs:");
+                inputs.iter().for_each(|a| print!(" {},", a));
+                println!();
+
+                //outputs
+                print!("Outputs:");
+                outputs.iter().for_each(|a| print!(" {},", a));
+                println!();
+
+                //elements
+                println!("Elements:");
+                print_blif_components(elems);
             }
-            _ => todo!(),
         }
     }
 }
