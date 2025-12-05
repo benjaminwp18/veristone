59b4649b4825481e272ef285886a0142eaf79741
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-12-05 01:11:39 -0500

    Support command block wireless redstone in mcfunction

--- res/logic_datapack/data/logic/function/build.mcfunction
+++ res/logic_datapack/data/logic/function/build.mcfunction
@@ -1,2 +1,6 @@
 place template logic:and_gate ~0 ~ ~0
-place template logic:not_gate ~1 ~ ~4
+place template logic:not_gate ~0 ~ ~8
+setblock ~1 ~ ~4 minecraft:redstone_lamp
+setblock ~0 ~ ~7 minecraft:target
+setblock ~0 ~-1 ~7 minecraft:repeating_command_block{auto: 1b, Command: "execute if block ~1 ~1 ~-3 minecraft:redstone_lamp[lit=true] run setblock ~ ~1 ~ minecraft:redstone_block"}
+setblock ~0 ~-2 ~7 minecraft:repeating_command_block{auto: 1b, Command: "execute if block ~1 ~2 ~-3 minecraft:redstone_lamp[lit=false] run setblock ~ ~2 ~ minecraft:target"}
--- src/bin/mcfunction.rs
+++ src/bin/mcfunction.rs
@@ -8,6 +8,29 @@ struct Gate {
     z: i32
 }
 
+struct Wire {
+    start_x: i32,
+    start_z: i32,
+    end_x: i32,
+    end_z: i32
+}
+
+// PlacementAlgo::DumbGrid { num_cols: 4 }, RoutingAlgo::Wireless
+
+// enum PlacementAlgo {
+//     DumbGrid { num_cols: i32 },
+//     // TimberWolf
+// }
+
+// enum RoutingAlgo {
+//     Wireless
+// }
+
+enum WireType {
+    Wireless,
+    Redstone
+}
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -15,6 +38,7 @@ struct Args {
     src: String
 }
 
+const MCFUNCTION_PATH: &'static str = "res/logic_datapack/data/logic/function/build.mcfunction";
 
 #[allow(dead_code)]
 fn main() {
@@ -27,22 +51,59 @@ fn main() {
     };
     let not_gate: Gate = Gate {
         name: String::from("not"),
-        x: 1,
-        z: 4
+        x: 0,
+        z: 8
     };
     let gates: Vec<Gate> = vec![
         and_gate,
         not_gate
     ];
 
-    create_file(gates);
+    let wires: Vec<Wire> = vec![
+        Wire {
+            start_x: 1,
+            start_z: 4,
+            end_x: 0,
+            end_z: 7
+        }
+    ];
+
+    write_mcfunction(&gates, &wires, WireType::Wireless);
 }
 
-fn create_file (gates: Vec<Gate>) {
-    let path: &Path = Path::new("res/logic_datapack/data/logic/function/build.mcfunction");
+fn write_mcfunction(
+    gates: &Vec<Gate>,
+    wires: &Vec<Wire>,
+    wire_type: WireType
+) {
+    let path: &Path = Path::new(MCFUNCTION_PATH);
     let mut file = File::create(path).unwrap();
+    let file_error: &str = &format!("Failed to write gate to mcfunction file at {MCFUNCTION_PATH}");
 
-    for gate in &gates {
-        let _ = file.write(format!("place template logic:{}_gate ~{} ~ ~{}\n", gate.name, gate.x, gate.z).as_bytes());
+    for gate in gates {
+        writeln!(file, "place template logic:{}_gate ~{} ~ ~{}", gate.name, gate.x, gate.z).expect(file_error);
     }
+
+    for wire in wires {
+        match wire_type {
+            WireType::Redstone => {
+                panic!("TODO: straight-line only redstone wire??");
+                // writeln!(file, "fill ~{} ~ ~{} ~{} ~ ~{} minecraft:redstone_wire", wire.start_x, wire.start_z, wire.end_x, wire.end_x).expect(file_error);
+            },
+            WireType::Wireless => {
+                let relative_start_x = wire.start_x - wire.end_x;
+                let relative_start_z = wire.start_z - wire.end_z;
+
+                let on_command = format!("execute if block ~{relative_start_x} ~1 ~{relative_start_z} minecraft:redstone_lamp[lit=true] run setblock ~ ~1 ~ minecraft:redstone_block");
+                let off_command = format!("execute if block ~{relative_start_x} ~2 ~{relative_start_z} minecraft:redstone_lamp[lit=false] run setblock ~ ~2 ~ minecraft:target");
+
+                writeln!(file, "setblock ~{} ~ ~{} minecraft:redstone_lamp", wire.start_x, wire.start_z).expect(file_error);
+                writeln!(file, "setblock ~{} ~ ~{} minecraft:target", wire.end_x, wire.end_z).expect(file_error);
+                writeln!(file, "setblock ~{} ~-1 ~{} minecraft:repeating_command_block{{auto: 1b, Command: \"{on_command}\"}}", wire.end_x, wire.end_z).expect(file_error);
+                writeln!(file, "setblock ~{} ~-2 ~{} minecraft:repeating_command_block{{auto: 1b, Command: \"{off_command}\"}}", wire.end_x, wire.end_z).expect(file_error);
+            }
+        }
+    }
+
+    println!("Successfully wrote mcfunction to {MCFUNCTION_PATH}");
 }
