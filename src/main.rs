0adb3a179a60e0948c3624912b3cb5c5f92044e8
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2026-03-27 16:33:55 -0400

    Add Y coordinates to gate pins & use Lee

--- src/main.rs
+++ src/main.rs
@@ -19,5 +19,5 @@ fn main() {
     let blif_path = blif_buf.as_path();
     make_blif::generate_blif(verilog_path, Path::new("res/mc.lib"), blif_path, false);
     let (gates, wires) = read_blif::read_blif(blif_path, read_blif::PlacementAlgo::DumbGrid { num_cols: 4 });
-    mcfunction::write_mcfunction(&gates, &wires, mcfunction::RoutingAlgo::Wireless);
+    mcfunction::write_mcfunction(&gates, &wires, mcfunction::RoutingAlgo::Lee);
 }
